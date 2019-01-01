79bd1a40302ee061731f9d983c566dfa5a8fddae
takarj <tkrjagd@gmail.com>
2019-01-01 12:13:57 +0100

    server

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -3,18 +3,25 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.net.URLDecoder;
+
 public class DatabaseHandler{
     
     private String locationPath;
     private static String DATABASE_NAME = "toilets";
     
     public DatabaseHandler(){
-        locationPath = getClass().getClassLoader().getResource("").getPath();
+        locationPath = getClass().getClassLoader().getResource("").getPath();  // /G:/Documents/%23SCHOOL/P-Seminar%20Inf/Toilets%20Server/
+        //System.out.println(URLDecoder.decode(locationPath)); 
         init();
     }
     
     private void init(){
-        String dbUrl = "jdbc:sqlite:" + locationPath + DATABASE_NAME + ".db";
+        String dbUrl = "jdbc:sqlite:" + prepareUrlString(locationPath) + DATABASE_NAME + ".db";
+        
+        //String dbUrl = "jdbc:sqlite:G:\\Documents\\#SCHOOL\\P-Seminar Inf\\Toilets Server\\toilets.db";
+        
+        //String dbUrl = "jdbc:sqlite:G:\\Documents\\%23SCHOOL\\P-Seminar%20Inf\\Toilets%20Server\\toilets.db";
         
         try (Connection conn = DriverManager.getConnection(dbUrl)) {
             if (conn != null) {
@@ -26,5 +33,19 @@ public class DatabaseHandler{
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+    
+    public static String prepareUrlString(String u){
+        String url = "";
+        
+        u = URLDecoder.decode(u);
+        
+        String[] splitted = u.split("\\/", -1);
+        
+        for(int i = 1; i < splitted.length; i++){
+            url = url + splitted[i] + "\\";
+        }
+        
+        return url;
     }
 }
